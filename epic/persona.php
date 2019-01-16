9d64a582f8f05238c6d1d6bdbd6da6f77388a549
Nehomah Mora <nehomahm@gmail.com>
2019-01-16 12:01:51 -0700

    created epic dir with 3 php pages

---
+++ epic/persona.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="=UTF-8">
+		<title>Epic Persona, Usecase & Flow</title>
+	</head>
+	<body>
+		<div>
+
+		</div>
+	</body>
---
+++ epic/usecase.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Admin
+ * Date: 1/16/2019
+ * Time: 11:59 AM
+ */
