5f189b97d9a78ffcca5ef85ba4a88d7bc3f8f982
Nehomah Mora <nehomahm@gmail.com>
2019-01-16 16:11:27 -0700

    added link ref for page 2 and 3

--- epic/index.php
+++ epic/index.php
@@ -8,4 +8,8 @@
 		<h1>Data Design Project</h1>
 
 		<h2><a href="./roger-nelson.php">Persona</a></h2>
+
+		<h3><a href="use-case.php">Use Case</a></h3>
+
+		<h4><a href="conceptual-model.php">Conceptual Model</a></h4>
 	</body>
