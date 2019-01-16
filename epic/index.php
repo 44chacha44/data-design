7ebb9623856ef3ae8999e6d99ec602fdb4a44e25
Nehomah Mora <nehomahm@gmail.com>
2019-01-16 16:06:15 -0700

    switched persona code from index to roger-nelson

--- epic/index.php
+++ epic/index.php
@@ -5,49 +5,7 @@
 		<title>Data Design Project</title>
 	</head>
 	<body>
-
 		<h1>Data Design Project</h1>
 
-		<h2>User Persona</h2>
-
-		<ul>
-			<li><strong>Name:</strong> Roger Nelson</li>
-			<li><strong>Age:</strong> 60</li>
-			<li><strong>Ethnicity:</strong> Black</li>
-			<li><strong>Occupation:</strong> Retired Musician</li>
-			<li><strong>Technology:</strong>
-				<ul>
-					<li>Desktop is iMac Pro running Mojave</li>
-					<li>Phone is iPhone Xs on 12.1.2</li>
-				</ul>
-			</li>
-			<li><strong>Hobbies:</strong>
-				<ul>
-					<li>Dancing</li>
-					<li>Singing</li>
-					<li>Basketball</li>
-					<li>Composing Music</li>
-				</ul>
-			</li>
-			<li><strong>Needs/Goals/Features:</strong></li>
-		<ul>
-					<li>Shopping for new bed that is non-toxic and supportive</li>
-					<li>Wants the process to be quick</li>
-					<li>Goal is to get a good nights rest on a bed fit for a Prince</li>
-		</ul>
-			<li><strong>Frustrations:</strong></li>
-		<ul>
-					<li>Doesn't like being overwhelmed with information</li>
-					<li>Wants to make sure bed helps him before committing to purchase</li>
-		</ul>
-		</ul>
-
-		<h2>User Story</h2>
-		<p>Mr. Nelson need a comfortable bed to finally get a good nights sleep as quickly as possible.</p>
-
-		<div>
-			<img src="prince1.jpg" alt="prince">
-		</div>
-
+		<h2><a href="./roger-nelson.php">Persona</a></h2>
 	</body>
-</html>
