53bc7fc3fd1e3fb59ce94332454ba9d7cb288f46
Nehomah Mora <nehomahm@gmail.com>
2019-01-16 14:29:49 -0700

    added User story

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -3,5 +3,5 @@
  * Created by PhpStorm.
  * User: Admin
  * Date: 1/16/2019
- * Time: 11:59 AM
+ * Time: 1:20 PM
  */
---
+++ epic/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Data Design Project</title>
+	</head>
+	<body>
+
+		<h1>Data Design Project</h1>
+
+		<h2>User Persona</h2>
+
+		<ul>
+			<li><strong>Name:</strong> Roger Nelson</li>
+			<li><strong>Age:</strong> 60</li>
+			<li><strong>Ethnicity:</strong> Black</li>
+			<li><strong>Occupation:</strong> Retired Musician</li>
+			<li><strong>Technology:</strong> <em>Desktop</em>; iMac Pro Mojave <em>Phone</em>; iPhone Xs 12.1.2 </li>
+			<li><strong>Hobbies:</strong> <em>Dancing</em>; <em>Singing</em>; <em>Basketball</em>; <em>Composing Music</em></li>
+			<li><strong>Needs/Goals/Features:</strong></li>
+		<ul>
+					<li>Shopping for new bed that is non-toxic and supportive</li>
+					<li>Wants the process to be quick</li>
+					<li>Goal is to get a good nights rest on a bed fit for a Prince</li>
+		</ul>
+			<li><strong>Frustrations:</strong></li>
+		<ul>
+					<li>Doesn't like being overwhelmed with information</li>
+					<li>Wants to make sure bed helps him before committing to purchase</li>
+		</ul>
+		</ul>
+
+		<h2>User Story</h2>
+		<p>Mr. Nelson need a comfortable bed to finally get a good nights sleep as quickly as possible.</p>
+
+	</body>
+</html>
