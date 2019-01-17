f62df9ea53411401ac598d34acbf2e312cad7def
Nehomah Mora <nehomahm@gmail.com>
2019-01-17 14:05:39 -0700

    added more server responses to interactive flow

--- epic/conceptual-model.php
+++ epic/conceptual-model.php
@@ -1,1 +1,51 @@
-<h1>Conceptual Model</h1>
+<body>
+	<h1>Conceptual Model</h1>
+
+	<h2>Entities & Attributes:</h2>
+
+	<p><strong>Mattresses</strong></p>
+
+	<ul>
+		<li>mattressId</li>
+		<li>reviewRate</li>
+		<li>mattressSize</li>
+		<li>mattressComfortLevel</li>
+		<li>freeGift</li>
+		<li>sheetColor</li>
+		<li>mattressPrice</li>
+	</ul>
+
+	<p><strong>Sheets</strong></p>
+
+	<ul>
+		<li>sheetsId</li>
+		<li>reviewRate</li>
+		<li>sheetsSize</li>
+		<li>sheetsPrice</li>
+	</ul>
+
+	<p><strong>Reviews</strong></p>
+
+	<ul>
+		<li>productId</li>
+		<li>productIdImage</li>
+		<li>reviewRateAverage</li>
+		<li>reviewerId</li>
+		<li>reviewedDate</li>
+		<li>reviewTitle</li>
+		<li>reviewContent</li>
+		<li>reviewerSleepStyle</li>
+		<li>helpfulCount</li>
+	</ul>
+
+	<p><strong>Support</strong></p>
+
+	<ul>
+		<li>productId</li>
+		<li>productQuestions</li>
+		<li>contactOptions</li>
+
+
+	</ul>
+
+</body>
