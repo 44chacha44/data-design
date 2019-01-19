9c307ef09f0a038489dac095c17fd5640cd3dfc5
Nehomah Mora <nehomahm@gmail.com>
2019-01-18 22:03:08 -0700

    changed title of ddp, added sql configuration again

--- epic/roger-nelson.php
+++ epic/roger-nelson.php
@@ -1,6 +1,6 @@
 <body>
 
-	<h1>Data Design Project</h1>
+	<h1>Nehomah's Data Design Project</h1>
 
 	<h2>User Persona</h2>
 
@@ -24,17 +24,17 @@
 			</ul>
 		</li>
 		<li><strong>Needs/Goals/Features:</strong></li>
-		<ul>
-			<li>Shopping for new bed that is non-toxic and supportive</li>
-			<li>Wants the process to be quick</li>
-			<li>Goal is to get a good nights rest on a bed fit for a Prince</li>
-		</ul>
+			<ul>
+				<li>Shopping for new bed that is non-toxic and supportive</li>
+				<li>Wants the process to be quick</li>
+				<li>Goal is to get a good nights rest on a bed fit for a Prince</li>
+			</ul>
 		<li><strong>Frustrations:</strong></li>
-		<ul>
-			<li>Doesn't like being overwhelmed with information</li>
-			<li>Wants to make sure bed helps him before committing to purchase</li>
+			<ul>
+				<li>Doesn't like being overwhelmed with information</li>
+				<li>Wants to make sure bed helps him before committing to purchase</li>
+			</ul>
 		</ul>
-	</ul>
 
 	<h2>User Story</h2>
 	<p>Mr. Nelson need a comfortable bed to finally get a good nights sleep as quickly as possible.</p>
@@ -44,4 +44,3 @@
 	</div>
 
 </body>
-</html>
